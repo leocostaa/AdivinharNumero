a26e4f98d8950432ab1885388bd39ace36ae780b
leocostaa <32634126+leocostaa@users.noreply.github.com>
2021-05-07 10:27:41 +0100

    Ler o número e verificar se é valido

--- app/src/main/java/pt/ipg/adivinharnumero/MainActivity.kt
+++ app/src/main/java/pt/ipg/adivinharnumero/MainActivity.kt
@@ -2,15 +2,50 @@ package pt.ipg.adivinharnumero
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.TextView
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    private val random = Random()
+    private var numeroAdivinhar: Int = 0
+    private var jogo: Int = 0
+    private var tentativas: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        novoJogo()
     }
-    val textInputEditIdade = findViewById<EditText>(R.id.EditTextNumero)
-    val NumeroCerto = 5
+
+    private fun novoJogo(){
+        numeroAdivinhar = random.nextInt(10) + 1
+        jogo++
+        tentativas = 0
+        atualizaJogoTentativas()
+    }
+
+    private fun atualizaJogoTentativas() {
+        findViewById<TextView>(R.id.textViewJogo).text = getString(R.string.jogo) + jogo
+        findViewById<TextView>(R.id.textViewTentativas).text = getString(R.string.tentativa) + tentativas
+    }
+
+    fun adivinha(view: View) {
+        val editTextNumero = findViewById<EditText>(R.id.EditTextNumero)
+        val numero = editTextNumero.text.toString().toIntOrNull()
+        when (numero) {
+            in 1..10 -> verificaAcertou()
+            null -> editTextNumero.error = getString(R.string.numero_invalido)
+            else -> editTextNumero.error = getString(R.string.numero_1e10)
+
+        }
+    }
+
+    private fun verificaAcertou() {
+        TODO("Not yet implemented")
+    }
 
 
 }
